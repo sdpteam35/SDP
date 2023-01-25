7b2d8b1fb784a6ef885dbc6b11ff80b6c1f57a6c
bazz-c <john.b.coleman@uconn.edu>
2023-01-25 17:13:03 -0500

    BazzWorld Updates

--- BazzWorld.java
+++ BazzWorld.java
@@ -1,5 +1,7 @@
 public class BazzWorld {
     public static void main(String [] commands) {
         System.out.println("Welcome to my world");
+        int x = 125;
+        System.out.println(x);
     }
 }
