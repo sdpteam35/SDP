b4d912c666b159adcca0c67f592418c097d2d36e
Person1507 <brownryan1022@gmail.com>
2023-03-08 18:19:04 -0500

    Made grid interactive Not complete (there is only a circle that you can move around) Grid isn't centered, work in progress

--- src/circuitryapp/Main.java
+++ src/circuitryapp/Main.java
@@ -1,9 +1,13 @@
 package circuitryapp;
 
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
+import javafx.geometry.VPos;
 import javafx.scene.ImageCursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -14,9 +18,13 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -25,6 +33,14 @@ public class Main extends Application {
 
     private Node circuitElement;
     private Scene scene;
+    private int squareSize = 75;
+    private int gridHeightSquares = 10;
+    private int gridWidthSquares = 5;
+    private int gridHeight = squareSize * gridHeightSquares;
+    private int gridWidth = squareSize * gridWidthSquares;
+    private int numSquares = gridHeightSquares * gridWidthSquares;
+    private int gridSize = squareSize * gridHeightSquares;
+    Rectangle[][] gridMatrix;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -53,21 +69,49 @@ public class Main extends Application {
         RunMenu.getItems().addAll(runmenu1);
         menubar.getMenus().addAll(FileMenu, EditMenu, SelectionMenu, ViewMenu, RunMenu, HelpMenu);
 
+        Pane grid = new Pane();
+        //GridPane grid = new GridPane();
+        /*
+        for (int i=0; i<gridWidth; i++) {
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setValignment(VPos.CENTER);
+            grid.getRowConstraints().add(rowConstraints);
+
+            ColumnConstraints colConstraints = new ColumnConstraints();
+            colConstraints.setHalignment(HPos.CENTER);
+            grid.getColumnConstraints().add(colConstraints);
+        }
+        */
+
+
         // Grid
-        GridPane grid = new GridPane();
-        int height = 5;
-        int width = 10;
-        grid.setAlignment(Pos.CENTER);
-        Rectangle[][] gridMatrix = new Rectangle[height][width];
-        for(int i = 0; i < height; i++) {
-            for(int j = 0; j < width; j++) {
-                Rectangle rect = new Rectangle(75, 75);
+        ArrayList<Square> squares = new ArrayList<Square>();
+        gridMatrix = new Rectangle[gridHeightSquares][gridWidthSquares];
+        for(int i = 0; i < gridHeight; i += squareSize) {
+            for(int j = 0; j < gridWidth; j += squareSize) {
+                Rectangle rect = new Rectangle(i, j, squareSize, squareSize);
                 rect.setFill(Color.WHITE);
                 rect.setStroke(Color.BLACK);
-                gridMatrix[i][j] = rect;
-                grid.add(rect, j, i);
+                gridMatrix[i/squareSize][j/squareSize] = rect;
+                //squares[i/squareSize][j/squareSize] = new Square()
+                grid.getChildren().add(rect);
             }
         }
+
+        int radius = squareSize / 3;
+        Circle c = new Circle(radius);
+        c.setFill(Color.YELLOW);
+        c.setStroke(Color.BLUE);
+        int posX = squareSize / 2;
+        int posY = squareSize / 2;
+        Square square = new Square(posX, posY, radius, c);
+        squares.add(square);
+        grid.getChildren().add(c);
+        square.draw();
+
+        c.setOnMousePressed(event -> pressed(event, square));
+        c.setOnMouseDragged(event -> dragged(event, square));
+        c.setOnMouseReleased(event -> release(event, square));
 
         // Mouse coordinates label
         Label mouseCoord = new Label();
@@ -87,6 +131,7 @@ public class Main extends Application {
         Image image = new Image("file:src/circuitryapp/resistor.jpg", 75, 75, true, true);
         scene.setCursor(new ImageCursor(image));
 
+        //grid.setAlignment(Pos.CENTER);
         root.setCenter(grid);
         root.setTop(menubar);
         root.setBottom(new StackPane(mouseCoord));
@@ -98,4 +143,23 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public void pressed(MouseEvent event, Square square) {
+        square.setColor(Color.ORANGE);
+    }
+
+    public void dragged(MouseEvent event, Square square) {
+        square.setX(square.getX() + event.getX());
+        square.setY(square.getY() + event.getY());
+        square.draw();
+    }
+
+    public void release(MouseEvent event, Square square){
+        int gridX = (int)square.getX() / squareSize;
+        int gridY = (int)square.getY() / squareSize;
+        square.setX(squareSize/2 + squareSize * gridX);
+        square.setY(squareSize/2 + squareSize * gridY);
+        square.draw();
+    }
+
 }
